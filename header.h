251bae357fcc623a3ba209816f299b8ba236b847
ZainabEman <zainabeman976@gmail.com>
2025-03-01 00:07:24 +0500

    updates

--- header.h
+++ header.h
@@ -1,425 +1,214 @@
-	//Libraries
-	#include <iostream>
-	#include<fstream>
-	#include <ctime>
-	#include<cstdlib>
-	#include<stdlib.h>
-    #include<windows.h>
-	
-	
-	using namespace std;
-	//Global Arrays:
-	int goal[5][5];
-	int initial[5][5];
-	char path[1000]={'\0'};
-	
-	//Global Variable:
-	int x;                                //row index for 25th tile
-	int y;                               //coloumn index for 25th tile
-	int z=0;                            //variable to store moves in path[] 
-	int total_count=0;                 //total legal moves takes by user        
-	
-	//Functions decleration:
-	void init_();
-	void goal_();
-	void display_goal();
-	void display_initial();
-	bool is_solvable(int a1[5][5],int a2[5][5]);
-	bool is_goal(int a3[5][5],int a4[5][5]);
-	int legal_moves();
-	void make_move();
-	void print_path();
-	
-	
-	void init_()
-	{
-		int num;
-		
-		for(int i=0;i<5;i++)
-		{
-			for(int j=0;j<5;j++)
-			{
-				initial[i][j]=0;
-			}
-		}
-		for(int i=0;i<5;++i)
-		{
-		for(int j=0;j<5;++j)
-		{
-		    Za:
-		    num=rand()%25+1;                   //generation of random numbers
-		    initial[i][j]=num;
-		
-		    for(int k=0;k<5;++k)
-		    {
-		        for(int x=0;x<5;++x)
-		        {
-		        	if(num==initial[k][x])
-		        	{
-		        		if(x==j&&k==i)
-		        		{
-		        			
-						}
-						else
-						{
-							goto Za;
-						}
-					}
-				}
-		   	}    
-		}
-	
-	}
-	  	 
-	}
-	void goal_()
-	{
-	  int num;
-	  for(int i=0;i<5;++i)
-	  {
-	  	for(int j=0;j<5;++j)
-	  	{
-	  	    Za:
-	  	    num=rand()%25+1;
-	  	    goal[i][j]=num;
-	  	
-	  	    for(int k=0;k<5;++k)
-		    {
-		        for(int x=0;x<5;++x)
-		        {
-		        	if(num==goal[k][x])
-		        	{
-		        		if(x==j&&k==i)
-		        		{
-		        			
-						}
-						else
-						{
-							goto Za;
-						}
-					}
-				}
-	       	}    
-		}
-	  
-	  }
-	}
-	void display_goal()
-	{  
-	 cout<<"\tGoal state is : "<<endl;
-		for(int i=0;i<5;i++)
-		 {
-		 	cout<<"\t\t\t\t";
-		 	for(int j=0;j<5;++j)
-		 	{
-		 	    if(goal[i][j]==25)
-				 {
-				 cout<<" "<<"\t";
-				 x=i;
-				 y=j;
-	
-				 }	
-				 else
-				 {
-				  cout<<goal[i][j]<<"\t";
-				 }
-			}
-			cout<<endl<<endl;
-		 }
-		}
-	void display_initial()
-	{   
-	    cout<<"\t Initial state is : "<<endl;
-		for(int i=0;i<5;i++)
-		 {
-		 	cout<<"\t\t\t\t";
-		 	for(int j=0;j<5;++j)
-		 	{
-		 	    if(initial[i][j]==25)
-				 {
-				 cout<<" "<<"\t";
-				 x=i;
-				 y=j;
-	
-				 }	
-				 else
-				 {
-				  cout<<initial[i][j]<<"\t";
-				 }
-			}
-			cout<<endl<<endl;
-		 }
-		}
-	bool is_solvable(int a1[5][5],int a2[5][5])	
-	{
-			 int count1=0,count2=0,temp;
-	     for(int i=0;i<5;i++)                  //will count inverstion in initial state
-		   {
-		 	for(int j=0;j<5;++j)
-		 	{
-		 		temp=a1[i][j];
-		 	     for(int k=i+1;k<5;++k)
-		 	    {
-		 	     for (int x=j+1;x<5;++x)
-				  {
-				  	if (temp>a1[k][x])
-				  	{
-				  		++count1;	
-					}
-				  }	
-				}
-			}
-		}
-	 
-		for(int i=0;i<5;i++)                  //will count inverstion in goal state
-		   {
-		 	for(int j=0;j<5;++j)
-		 	{
-		 		temp=a2[i][j];
-		 	     for(int k=i+1;k<5;++k)
-		 	    {
-		 	     for (int x=j+1;x<5;++x)
-				  {
-				  	if (temp>a2[k][x])
-				  	{
-				  	++count2;	
-					}
-				  }	
-				}
-			}
-		    }	
-		if(count1%2==0)      //will return true if and only idf inversion of both arrays will be even
-		{
-		   if(count2%2==0)
-		    {
-			  return true;
-	     	} 
-	     	else
-	    	{
-			  return false;
-	    	}
-	    }
-	    else if(count1%2==1)    //will return true if and only idf inversion of both arrays will be odd
-		{
-		   if(count2%2==1)
-		    {
-			  return true;
-	     	} 
-	     	else
-	    	{
-			  return false;
-	    	}
-	    } 
-	}
-	int legal_moves()
-	{
-		// system("Color f6");
-		bool n=false;
-	  if(initial[0][0]==25)	
-	  {
-	  	cout<<" Legal Moves are : \n\t Left \n\t Up \n ";        //will tell legal moves if space is at upper left corner
-	  	n=true;
-	  }
-	  else if(initial[0][4]==25)	                             //will tell legal moves if space is at upper right corner
-	  {
-	  	cout<<" Legal Moves are : \n\t Right \n\t Up \n ";
-	  	n=true;
-	  }
-	  else if(initial[4][0]==25)	                             //will tell legal moves if spaces is at lower left corner
-	  {
-	  	cout<<" Legal Moves are : \n\t Down \n\t Left \n ";
-	  	n=true;
-	  }
-	  else if(initial[4][4]==25)	                             //will tell legal moves if space is at lower right corner
-	  {
-	  	cout<<" Legal Moves are : \n\t Down \n\t Right \n ";
-	  	n=true;
-	  }
-	  else if(1)
-	  {
-	  	for(int j=1;j<4;++j)                                     //will tell legal moves if space in the first row except corner
-	  {
-	  	 if(initial[0][j]==25)	
-	  {
-	  	cout<<" Legal Moves are : \n\t Left \n\t Right \n\t Up \n ";
-	  	n=true;
-	  	break;
-	  	
-	  }
-	     else if(initial[4][j]==25)                              //will tell legal moves if space in the last row except corner	
-	  {
-	  	cout<<" Legal Moves are : \n\t Left \n\t Right \n\t Down \n ";
-	  	n=true;
-	  	break;
-	  }
-	  }
-	  for(int i=1;i<4;++i)                                      //will tell legal moves if space in the first coloumn except corner	
-	  {   
-	   if(initial[i][0]==25)	
-	  {
-	  	cout<<" Legal Moves are : \n\t Left \n\t Up \n\t Down \n ";
-	  	n=true;
-	  	break;
-	  } 
-	     else if(initial[i][4]==25)	                            //will tell legal moves if space in the last coloumn except corner	
-	  {
-	  	cout<<" Legal Moves are : \n\t Right \n\t Up \n\t Down \n ";
-	  	n=true;
-	  	break;
-	  }
-	  }
-	  }
-	  if(n==false)                                              //will tell legal move for all other positions
-	  {
-	  	cout<<" Legal Moves are : \n\t Left \n\t Right \n\t Up \n\t Down \n ";
-	  }
-	  
-	}
-	void make_move()
-	{
-		char choice;
-		int temp=0;
+#ifndef PUZZLE_H
+#define PUZZLE_H
 
-	    cout<<"\n Enter Direction : ";
-	    cin>>choice;
-	    path[z]=choice;                                                        // will store moves in path[] to print later
-	    ++z;
-	    total_count++;
-	    switch(choice)
-	    {
-	    	case'u':                                                            // case to move the tile upward
-	    	
-			case 'U': 
-		    	{
-		    		x++;
-		    		if(x!=5)
-					{
-						temp=initial[x][y];
-			    		initial[x][y]=initial[--x][y];
-			    		initial[x][y]=temp;
-					}
-					break;	
-				}
-			
-	    	
-			case'd':                                                            //case to move the tile downward	
-		    case'D':
-	    		{
-				 x--;
-	    		 if(x!=-1)
-	    		 {
-	    		 	temp=initial[x][y];
-		    		 initial[x][y]=initial[++x][y];
-		    		 initial[x][y]=temp;
-				 }
-				break;
-				}
-			
-			case'r':                                                            //case to move the tile right
-		    case'R':
-	    		{
-	    		--y;
-	    		if(y!=-1)
-				{
-					temp=initial[x][y];
-				initial[x][y]=initial[x][++y];
-				initial[x][y]=temp;
-				
-				}
-				break;	
-				}
-			
-			case'l':                                                            //case to move the tile left
-			case'L':
-	    		{
-				 ++y;
-	    		if(y!=5)
-				{
-					temp=initial[x][y];
-					initial[x][y]=initial[x][--y];
-					initial[x][y]=temp;
-					
-				}
-				break;	
-				}
-			
-			default :                                                            //case to handle exception
-				{
-					cout<<"\n Wrong input\n";
-				}
-	  
-		}
-	}
-	bool is_goal(int a3[5][5],int a4[5][5])
-	{
-	   for(int i=0;i<5;i++)                                    //this function will return true if goal state is equal to initial state 
-		   {
-		 	for(int j=0;j<5;++j)
-		 	{
-		 	if(a3[i][j]!=a4[i][j])
-		 	{
-		 		return false;
-		 		break;
-			 }
-			}
-		
-		}  
-		cout<<"\n----------------------------------------------------------congratulations------------------------------------------------------------";
-		cout<<"\n--------------------------------------------------------You reached Goal state ---------------------------------------------------\n";
-		return true;	
-	}
-	void print_path()
-	{   
-	     
-	     ofstream oFile;
-		 oFile.open("Puzzle Report.txt",ios::out) ;
-		 oFile<<"Your Puzzle Report is as follow : ";
-	     oFile<<"\n\t Your total moves are : "<<total_count;
-	     oFile<<"\n\t Your moves from initial to goal state is as follow :   ";
-		cout<<"\n--------------------------------------------------------------- Your Puzzle Report is as follow -----------------------------------------------------------";
-	    cout<<"\n\t Your total moves are : "<<total_count;
-	    cout<<"\n\t Your moves from initial to goal state is as follow :   ";
-	 	for(int i=0;path[i]!='\0';++i)
-	 	{
-	 		switch(path[i])
-	 		{
-	 			case('u'):
-	 		    case('U'):
-	 		    	{
-	 		    		oFile<<"UP\t";
-	 		    		cout<<"UP\t";
-	 		    		break;
-					 }
-			    case('d'):
-	 		    case('D'):
-	 		    	{
-	 		    	    oFile<<"DOWN\t";
-	 		    		cout<<"DOWN\t";
-	 		    		break;
-					 }
-				case('l'):
-	 		    case('L'):
-	 		    	{
-	 		    		oFile<<"LEFT\t";
-	 		    		cout<<"LEFT\t";
-	 		    		break;
-					 }
-				case('r'):
-	 		    case('R'):
-	 		    	{
-	 		    		oFile<<"RIGHT\t";
-	 		    		cout<<"RIGHT\t";
-	 		    		break;
-					 }
-			 }
-	 		
-		 }
-	
-	    oFile.close();
-		cout<<"\n\tYou can see you game report in the text file";
-	    cout<<endl<<endl;
-	}
-	
-	
-	
- 	
+#include <array>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include <sstream>
+#include <cctype>
+
+// The Puzzle class encapsulates a 5x5 sliding puzzle game.
+// The blank tile is represented by the number (size*size) i.e. 25.
+class Puzzle {
+public:
+    static const int size = 5;
+
+    // Constructor: seeds the random generator and generates a random goal board.
+    Puzzle() : moveCount(0) {
+        std::random_device rd;
+        rng.seed(rd());
+        generateGoal();
+    }
+
+    // Generates a random goal board.
+    void generateGoal() {
+        goalBoard = generateRandomBoard();
+    }
+
+    // Generates a random initial board that is solvable.
+    void generateInitialRandom() {
+        do {
+            currentBoard = generateRandomBoard();
+        } while (!isSolvable());
+        movePath.clear();
+        moveCount = 0;
+    }
+
+    // Loads the initial board from a text file.
+    // The file should contain exactly 25 numbers.
+    bool loadInitialFromFile(const std::string& filename) {
+        std::ifstream infile(filename);
+        if (!infile) {
+            std::cerr << "Error opening file: " << filename << "\n";
+            return false;
+        }
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                if (!(infile >> currentBoard[i][j])) {
+                    std::cerr << "Error reading board data from file.\n";
+                    return false;
+                }
+        infile.close();
+        if (!isSolvable()) {
+            std::cerr << "The loaded puzzle is not solvable.\n";
+            return false;
+        }
+        movePath.clear();
+        moveCount = 0;
+        return true;
+    }
+
+    // Displays a given board.
+    void displayBoard(const std::array<std::array<int, size>, size>& board) const {
+        for (const auto& row : board) {
+            std::cout << "\t";
+            for (const auto& tile : row) {
+                if (tile == size * size)
+                    std::cout << "   \t";  // blank tile
+                else
+                    std::cout << tile << "\t";
+            }
+            std::cout << "\n\n";
+        }
+    }
+
+    // Displays both the goal board and the current board.
+    // Uses ANSI escape codes to clear the screen and reposition the cursor.
+    void displayBoards() const {
+        std::cout << "\033[2J\033[H";  // Clear screen and move cursor to home position
+        std::cout << "\nGoal State:\n";
+        displayBoard(goalBoard);
+        std::cout << "\nCurrent State:\n";
+        displayBoard(currentBoard);
+        std::cout << "\nLegal Moves: ";
+        for (const auto& move : getLegalMoves())
+            std::cout << move << " ";
+        std::cout << "\n";
+    }
+
+    // Returns the legal moves for the blank tile as a vector of strings.
+    std::vector<std::string> getLegalMoves() const {
+        std::vector<std::string> moves;
+        auto [row, col] = findBlank(currentBoard);
+        if (col > 0)             moves.push_back("Left");
+        if (col < size - 1)        moves.push_back("Right");
+        if (row > 0)             moves.push_back("Up");
+        if (row < size - 1)        moves.push_back("Down");
+        return moves;
+    }
+
+    // Attempts to perform a move given a direction character ('L', 'R', 'U', 'D'; case-insensitive).
+    // Returns true if the move is successful.
+    bool makeMove(char direction) {
+        direction = std::tolower(direction);
+        auto [row, col] = findBlank(currentBoard);
+        int newRow = row, newCol = col;
+        switch (direction) {
+            case 'l': newCol = col - 1; break;
+            case 'r': newCol = col + 1; break;
+            case 'u': newRow = row - 1; break;
+            case 'd': newRow = row + 1; break;
+            default:
+                std::cout << "Invalid input. Use L, R, U, or D.\n";
+                return false;
+        }
+        if (newRow < 0 || newRow >= size || newCol < 0 || newCol >= size) {
+            std::cout << "Move not allowed from current position.\n";
+            return false;
+        }
+        std::swap(currentBoard[row][col], currentBoard[newRow][newCol]);
+        movePath.push_back(direction);
+        ++moveCount;
+        return true;
+    }
+
+    // Checks whether the current board matches the goal board.
+    bool isSolved() const {
+        return currentBoard == goalBoard;
+    }
+
+    // Prints a report of the moves made to both the console and a text file.
+    void printReport(const std::string& reportFilename) const {
+        std::ofstream outFile(reportFilename);
+        if (!outFile) {
+            std::cerr << "Error creating report file.\n";
+            return;
+        }
+        std::ostringstream report;
+        report << "Puzzle Report:\n"
+               << "Total moves: " << moveCount << "\n"
+               << "Move sequence: ";
+        for (const auto& move : movePath) {
+            switch (move) {
+                case 'u': report << "UP "; break;
+                case 'd': report << "DOWN "; break;
+                case 'l': report << "LEFT "; break;
+                case 'r': report << "RIGHT "; break;
+                default:  report << move << " "; break;
+            }
+        }
+        const std::string reportStr = report.str();
+        outFile << reportStr;
+        outFile.close();
+        std::cout << "\n" << reportStr << "\n";
+        std::cout << "Detailed report available in: " << reportFilename << "\n";
+    }
+
+private:
+    std::array<std::array<int, size>, size> currentBoard;
+    std::array<std::array<int, size>, size> goalBoard;
+    std::vector<char> movePath;
+    int moveCount;
+
+    // Generates a random board (a random permutation of numbers 1 to size*size).
+    std::array<std::array<int, size>, size> generateRandomBoard() {
+        std::array<int, size * size> numbers;
+        for (int i = 0; i < size * size; ++i)
+            numbers[i] = i + 1;
+        std::shuffle(numbers.begin(), numbers.end(), rng);
+        std::array<std::array<int, size>, size> board{};
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                board[i][j] = numbers[i * size + j];
+        return board;
+    }
+
+    // Finds and returns the (row, column) position of the blank tile (represented by size*size).
+    std::pair<int, int> findBlank(const std::array<std::array<int, size>, size>& board) const {
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                if (board[i][j] == size * size)
+                    return { i, j };
+        return { -1, -1 }; // Should not occur.
+    }
+
+    // Counts inversions in a board (ignoring the blank tile).
+    int countInversions(const std::array<std::array<int, size>, size>& board) const {
+        std::vector<int> flat;
+        for (const auto& row : board)
+            for (const auto& tile : row)
+                flat.push_back(tile);
+        int invCount = 0;
+        for (size_t i = 0; i < flat.size(); ++i)
+            for (size_t j = i + 1; j < flat.size(); ++j)
+                if (flat[i] != size * size && flat[j] != size * size && flat[i] > flat[j])
+                    ++invCount;
+        return invCount;
+    }
+
+    // Determines if the current puzzle is solvable by comparing inversion parity with the goal board.
+    bool isSolvable() const {
+        int invCurrent = countInversions(currentBoard);
+        int invGoal = countInversions(goalBoard);
+        return (invCurrent % 2) == (invGoal % 2);
+    }
+
+    std::mt19937 rng{ std::chrono::steady_clock::now().time_since_epoch().count() };
+};
+
+#endif // PUZZLE_H
--- original.cpp
+++ original.cpp
@@ -1,97 +1,42 @@
- 	//Libraries
-	#include <iostream>
-	#include<fstream>
-	#include <ctime>
-	#include<cstdlib>
-	#include<stdlib.h>
-    #include<windows.h>
-	#include"header.h"
-	using namespace std;
-	
-	
-	//Main Function 
-	int main()
-	{   
-	    
-	    srand(time(0));
-		bool flag,flag2;
-		int choice,choice2;
-		goal_();  
-		display_goal();                                 //Print goal state 
-		cout<<endl<<endl;
-		cout<<"\n Enter 1 if you want to take the numbers from txt file\n enter 0 if you want to generate random numbers\n\n choice: ";
-		cin>>choice;
-		if(choice==0)
-		{
-		do                                              //randomly initialize initial state
-		{  
-			init_();                                   //initialize initial state
-		    flag=is_solvable(goal,initial);
-		    if(flag==true)                             //check to terminate the loop if initial state is solvable
-			{   
-				break;
-			}
-	    }
-	    while(1);
-	    }
-	    else if (choice==1)                            //will initialize initial state from text file
-	    {
-	    ifstream iFile;
-		iFile.open("input.txt");
-		if(!iFile)                                     //check to handle exception
-		{
-			cout<<" Error opening in file ";
-		}
-		else
-		{
-		for(int i=0;i<5;++i)
-		{
-			for (int j=0;j<5;++j)
-			{
-				iFile>>initial[i][j];
-			}
-		}
-	    }
-		iFile.close();
-		}
-		else                                            //check to handle exception
-		{
-		cout<<" Wrong input ";	
-		}
-	    display_initial();                             //print intial state 
-	    cout<<"\n\t\t\t----------------------------------Puzzle is Solvable-----------------------------------\n\n\n";
-	    cout<<"\n Enter \"L\" to move the tile to Left   ";                    //instructions
-		cout<<"\n Enter \"R\" to move the tile to Right   ";
-		cout<<"\n Enter \"D\" to move the tile to Down   ";
-	    cout<<"\n Enter \"U\" to move the tile to Up \n  ";
-		do
-		{
-		legal_moves();
-	    make_move();	   
-	    display_goal();                   //Print goal state                     
-	    cout<<"\n\n";
-	    cout<<"\t Initial state is : "<<endl;
-	    display_initial();
-	    cout<<"\n\tif you want to end the enter any number\n";
-        cout<<"\t press 0 to continue :";
-		cin>>choice2;
-		if(choice2!=0)
-		{
-		break;	
-		}
-	    else
-	    {
-	    	continue;
-		}
-		flag2=is_goal(initial,goal);
-	    }
-	    
-	    while(flag2==false);
-	    if(flag2!=true)
-	    {
-	    	cout<<"\n---------------------------------------------------------------You did not reached the goal state------------------------------------------------------\n";
-		}
-	    print_path();	                          //function to print the path after goal state is reached
-		
-	}
-	
+#include "header.h"
+#include <cctype>
+
+int main() {
+    Puzzle game;
+    int choice = 0;
+
+    std::cout << "Enter 1 to load puzzle from file (input.txt), or 0 for a random puzzle: ";
+    std::cin >> choice;
+    if (choice == 1) {
+        if (!game.loadInitialFromFile("input.txt")) {
+            std::cerr << "Failed to load puzzle from file. Exiting.\n";
+            return 1;
+        }
+    } else {
+        game.generateInitialRandom();
+    }
+
+    // Game loop
+    char moveInput;
+    while (true) {
+        game.displayBoards();
+
+        if (game.isSolved()) {
+            std::cout << "\nCongratulations! You reached the goal state.\n";
+            break;
+        }
+
+        std::cout << "Enter move (L/R/U/D) or Q to quit: ";
+        std::cin >> moveInput;
+        if (std::tolower(moveInput) == 'q') {
+            std::cout << "Game quit. Better luck next time!\n";
+            break;
+        }
+
+        if (!game.makeMove(moveInput))
+            std::cout << "Please enter a valid move.\n";
+    }
+
+    game.printReport("PuzzleReport.txt");
+    return 0;
+}
